02aeaeceb5321337e6186173f46740395a543bc5
Isaac <isaactan310@gmail.com>
2024-06-26 10:09:22 +0800

    doing last part of part 2

--- part-2/notes/src/App.jsx
+++ part-2/notes/src/App.jsx
@@ -1,67 +1,111 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import Note from './components/Note'
+import { Note, Notification } from './components/Note'
+import noteService from './services/notes'
+import './index.css'
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2024</em>
+    </div>
+  )
+}
 
 const App = () => {
-
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('a new note...')
-  const [showAll, setShowAll] = useState(true)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
-
-  const hook = () => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
-  }
-  
-  useEffect(hook, [])
-  
-  console.log('render', notes.length, 'notes')
-  const notesToShow = showAll
-  ? notes
-  : notes.filter(note => note.important)
-
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+  }, [])
 
   const addNote = (event) => {
     event.preventDefault()
-    // Create note object that will receive its contents from the component newNote state
     const noteObject = {
       content: newNote,
-      important: Math.random() < 0.5, // 50% chance to be marked as important
-      id: notes.length + 1,
+      important: Math.random() > 0.5,
     }
   
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    noteService
+      .create(noteObject)
+        .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
   }
+
+  const toggleImportanceOf = id => {
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+  
+    noteService
+      .update(id, changedNote)
+        .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+
+        alert(
+          `the note '${note.content}' was already deleted from server`
+        )
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
         </button>
-      </div>
+      </div>      
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         )}
       </ul>
       <form onSubmit={addNote}>
-        <input 
-        value ={newNote}
-        onChange={handleNoteChange}/>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
         <button type="submit">save</button>
-      </form>
+      </form> 
+      <Footer />
     </div>
   )
 }
--- part-2/notes/src/main.jsx
+++ part-2/notes/src/main.jsx
@@ -21,13 +21,17 @@ const notes = [
     important: true
   }
 ]
+const promise = axios.get('http://localhost:3001/notes')
 
+promise.then(response => {
+  console.log(response)
+})
 // axios.get('http://localhost:3001/notes').then(response => {
 //   const notes = response.data
 //   console.log(notes)
 // })
 
-const promise2 = axios.get('http://localhost:3001/foobar')
-console.log(promise2)
+// const promise2 = axios.get('http://localhost:3001/foobar')
+// console.log(promise2)
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
