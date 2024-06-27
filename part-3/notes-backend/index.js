9e40b69a4d7d6ad301f73c36fbc39d3effbac6fd
Isaac <isaactan310@gmail.com>
2024-06-27 16:13:01 +0800

    doing part-3

--- part-3/notes-backend/index.js
+++ part-3/notes-backend/index.js
@@ -28,16 +28,24 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = request.params.id
+  const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
-  
-
   if (note) {
     response.json(note)
   } else {
-    response.status(404).end()
+    response.status(404).send('Does not exist!')
+    // response.status(404).end()
   }
 })
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
