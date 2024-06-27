a587785e1ff36e6fbfdc491a9dd84415f8b612f7
Isaac <isaactan310@gmail.com>
2024-06-27 15:48:29 +0800

    started part 3

---
+++ part-3/notes-backend/index.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const http = require('http') // same as 'import http from 'http' '
+
+
+const app = express()
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+// creates a new web server
+// event handler is register to server every time http req is made to address
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+  
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
