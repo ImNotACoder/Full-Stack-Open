a61db38f2aa98ecaaf389a373b52e4060e7d9175
Isaac <isaactan310@gmail.com>
2024-07-01 12:57:20 +0800

    working on part3

--- part-3/notes-backend/index.js
+++ part-3/notes-backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const http = require('http') // same as 'import http from 'http' '
+const cors = require('cors')
 
+app.use(cors())
 
 const app = express()
 app.use(express.json())
@@ -79,7 +81,7 @@ app.post('/api/notes', (request, response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
