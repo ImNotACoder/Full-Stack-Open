5272b715ce27ba355c2fe483072c0bca64aa62fd
Isaac <isaactan310@gmail.com>
2024-07-02 14:41:37 +0800

    updated configs

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import {Filter, Form, Persons} from './components/Component.jsx'
 import phonebookService from './services/phonebookrecords.js'
 
